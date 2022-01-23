c7c86a2d1830bcfcb7a0804bacf2f60eb4a9d243
MouseNymous <50442205+MouseNymous@users.noreply.github.com>
2022-01-23 09:43:10 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -119,8 +119,8 @@ async def repeat(event):
 async def repo_is_here(event):
     xx = await edit_or_reply(event, "`Processing...`")
     await xx.edit(
-                   ​f"╰⎆**Halo Saya Adalah Afi-Userbot**🇮🇩"
-                    f"❏**Owner​ ⎆ Tuan Muda Afi**"
+                   ​f"╰⎆**__Halo Saya Adalah Afi-Userbot__**🇮🇩\n"
+                    f"❏**Owner​ ⎆ **Tuan Muda Afi**\n"
                     f"❏**Channel** ⎆ [Malesin](t.me/rftnjg)\n"
                     f"❏**Group** ⎆ [Cari Kawan](t.me/ckbersama)\n"
                                 f"⎆ [Teman Telegram ID](https://t.me/temantelegram_id)\n"
